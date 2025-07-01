8d156c60c378bb5279b9c2a59cf100806cfd6ee2
sabineri85 <sabineri@hotmail.co.uk>
2025-07-01 15:05:56 +0200

    Initial commit

---
+++ Src/FundaTechnicalAssessment/App.cs
@@ -0,0 +1,32 @@
+﻿using FundaTechnicalAssessment.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundaTechnicalAssessment
+{
+    public class App(IPropertiesService propertiesService)
+    {
+        public async Task RunAsync()
+        {
+            var isValid = false;
+            Console.WriteLine("Please type the name of a city in the Netherlands.");
+            ConsoleKeyInfo keyReadCity = System.Console.ReadKey();
+            ConsoleKeyInfo keyReadGarden = System.Console.ReadKey();
+            do
+            {
+                Console.WriteLine("With garden?: type y or n");
+                keyReadGarden = System.Console.ReadKey();
+
+            } while (!isValid);
+            
+
+            var hasGarden = true ? keyReadGarden.ToString().Equals("y") : keyReadGarden.ToString().Equals("n");
+
+            var propertiesResult = await propertiesService.GetRankPropertiesByAgentAsync(keyReadCity.ToString(), hasGarden);
+        }
+    }
+
+}
---
+++ Src/FundaTechnicalAssessment/Program.cs
@@ -0,0 +1,30 @@
+﻿using FundaTechnicalAssessment;
+using FundaTechnicalAssessment.Core.Interfaces;
+using FundaTechnicalAssessment.Core.Services;
+using FundaTechnicalAssessment.ExternalIntegrations.HttpClients;
+using FundaTechnicalAssessment.ExternalIntegrations.Models.Settings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Http;
+using System.Configuration;
+
+var configuration = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)  // or Directory.GetCurrentDirectory()
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        // Register all services here
+        services.AddHttpClient<IFundaListingsProvider, FundaHttpClient>();
+        services.Configure<PropertySearchSettings>(configuration.GetSection(nameof(PropertySearchSettings)));
+        services.AddScoped<IPropertiesService, PropertiesService>();       
+    })
+    .Build();
+
+// Resolve and run the entry class (not Program.cs itself)
+var app = host.Services.GetRequiredService<App>();
+await app.RunAsync();
